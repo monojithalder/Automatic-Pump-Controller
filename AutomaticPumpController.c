c82ec12eadec5857308eac42c11f254b3fa6e111
monojithalder <monojithalder@hotmail.com>
2017-11-23 16:43:44 +0530

    Change logic

--- AutomaticPumpController.c
+++ AutomaticPumpController.c
@@ -6,37 +6,57 @@
 int main(void) {
 	
 	//Define Input Pins
-	pinMode('C',5,0); //reserver input
-	pinMode('C',4,0); //uppper tank low level
-	pinMode('C',3,0); //upper tank high level
+	pinMode('D',6,0); //reserver input
+	pinMode('B',0,0); //uppper tank low level
+	pinMode('D',7,0); //upper tank high level
 
 	//Define Output pins
-	pinMode('B',5,1); //enable transistor
-	pinMode('B',4,1); //enable led
+	pinMode('C',5,1); //enable transistor
+	pinMode('D',2,1); //enable led
 
 	
 	
 	int reserver_input = 0;
 	int tank_low_level_input = 0;
 	int tank_high_level_input = 0;
+	int pump_on = 0;
+	int reccived = 0;
 	while(1) {
 
 		//Read Inputs
-		reserver_input = digitalRead('C',5); //Read reserver input value
-		tank_low_level_input = digitalRead('C',4); //Read upper tank low level input value
-		tank_high_level_input = digitalRead('C',4); //Read upper tank high level input value
+		reserver_input = digitalRead('D',6); //Read reserver input value
+		tank_low_level_input = digitalRead('B',0); //Read upper tank low level input value
+		tank_high_level_input = digitalRead('D',7); //Read upper tank high level input value
 
 		if(reserver_input == 1) { //If reserver have enough water
-			if(tank_low_level_input == 0) {
-				//Pump On Condition Satisfy
-				digitalWrite('B',5,1);
-				digitalWrite('B',4,0);
-			}
-			else if(tank_low_level_input == 1 && tank_high_level_input == 1) {
+		   if(tank_low_level_input == 0 && tank_high_level_input == 1) {
 				//Pump off Condition satisfy
-				digitalWrite('B',5,0);
-				digitalWrite('B',5,1);
+				reccived = 0;
+				digitalWrite('C',5,0);
+				digitalWrite('D',2,1);
+				pump_on = 0;
 			}
+			else if(tank_low_level_input == 0 && tank_high_level_input == 0 && pump_on == 0) {
+				//Pump off Condition satisfy
+				reccived = 0;
+				digitalWrite('C',5,0);
+				digitalWrite('D',2,1);
+				pump_on = 0;
+			}
+			else if(tank_low_level_input == 1 || pump_on == 1) {
+				//Pump On Condition Satisfy
+				reccived++;
+				if(reccived > 500) {
+					digitalWrite('C',5,1);
+					digitalWrite('D',2,0);
+					pump_on = 1;
+				}
+			}
+		}
+		else {
+			digitalWrite('C',5,0);
+			digitalWrite('D',2,1);
+			pump_on = 0;
 		}
 	}
 	return 0;
